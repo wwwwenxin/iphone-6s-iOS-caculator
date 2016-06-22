f2116889081226cb6357891fac93272454000342
wwwwenxin <wx@admindeimac-2.local>
2016-06-22 16:46:58 +0800

    Initial Commit

---
+++ wxx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wxx
+//
+//  Created by wx on 16/6/22.
+//  Copyright © 2016年 wx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
