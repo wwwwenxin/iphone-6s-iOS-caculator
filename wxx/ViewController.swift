17ceec1094920db7109bca328a50b8f89e453ce9
wwwwenxin <wx@admindeiMac-2.local>
2016-06-22 17:43:56 +0800

    iOS caculator
    只定义了加减乘除，还未定义数字等

--- wxx/ViewController.swift
+++ wxx/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBAction func add(sender: AnyObject) {
+        var temp=0
+        temp = (Int)(x.text!)!+(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+    @IBAction func minus(sender: AnyObject) {
+        var temp=0
+        temp = (Int)(x.text!)!-(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+   
+    @IBAction func muliply(sender: AnyObject) {
+        var temp=0
+        temp = (Int)(x.text!)!*(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+    @IBAction func division(sender: AnyObject) {
+        var temp=0
+        temp = (Int)(x.text!)!/(Int)(y.text!)!
+        z.text="\(temp)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
